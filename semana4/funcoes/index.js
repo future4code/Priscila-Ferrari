9dc25f1a2e0144a42d8a5c8f06ed2b31d600564d
Priscila Carvalho <priferrari08@gmail.com>
2021-04-26 16:37:34 -0300

    questões feitas até a 4

---
+++ semana4/funcoes/index.js
@@ -0,0 +1,42 @@
+// Exercicio 1 - 
+// a)Vai ser impresso o resultado da multiplicação entre 2*5 e 10*5 em cada linha.
+// b) Não apareceria nada apesar da multiplicação ter sido feita
+
+// Exercicio 2 - 
+// a) Darvas // Caio // Como pede que imprima no console o array menor que numeração 2 então seria eles (0,1)
+// b) Seria impresso Amanda e Caio por que o restante da função se mantem pedindo o mesmo.
+
+// Exercicio 3 -
+
+
+// Exercicio 4 -
+// a)
+// function imprimirTexto(){
+//     console.log('Eu sou Priscila, tenho 36 anos, moro em Aracaju e estou between jobs')
+// }
+// imprimirTexto()
+
+// b)
+
+// let nome = prompt('Qual seu nome?')
+// let idade = Number (prompt('Qual sua idade?'))
+// let cidade = prompt('Qual cidade você mora?')
+// let respostaEstudante = Boolean (prompt('Você é estudante? Responda sim ou não.'))
+
+// if (respostaEstudante==="sim"){estudante = 'sou'
+// } else {
+//     estudante = 'não sou'
+// }
+// function imprimirTexto2 (){console.log(`Eu sou ${nome}, tenho ${idade}, moro em ${cidade} e ${estudante} estudante.`)
+
+// }
+// imprimirTexto2()
+
+//Exercício 5-
+
+let num1 = 'x'
+let num2 = 'y'
+
+function somaNumeros(num1,num2){
+    retnum1*num2
+}
