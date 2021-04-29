f39cff0d89fbd39b443df8e0eb2b2a9d92558d9a
Priscila Carvalho <priferrari08@gmail.com>
2021-04-28 23:05:22 -0300

    tentando finalizar

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -1,11 +1,40 @@
-const tituloPost 
-const autorPost 
-const conteudoPost 
+     
+  
 
-const blogEntrada = {
-titulo :,
-autor :,
-conteudo : ,
+    function mostrarConsole (){
+    const tituloPost =  document.getElementById('titulo-post').value
+    const autorPost = document.getElementById('autor-post').value
+    const conteudoPost =  document.getElementById('conteudo-post').value
+    const containerPost = ("container-de-posts")
+
+        const blogEntrada = {
+            titulo : (tituloPost),
+            autor :(autorPost),
+            conteudo : (conteudoPost)                    
+        }
+        console.log(blogEntrada)
+
+       
+            containerPost.innerHTML += `<p>${blogEntrada.titulo}</p> <p>${blogEntrada.autor}</p><p>${blogEntrada.conteudo}</p>`
+        
+    
+    
 }
 
-console.log(blog)
+// function AcrescentarPosts (){
+//     const tituloPost =  document.getElementById('titulo-post').value
+//     const autorPost = document.getElementById('autor-post').value
+//     const conteudoPost =  document.getElementById('conteudo-post').value
+//     const containerPost = ("container-de-posts")
+
+//     const blogEntrada = {
+//     titulo : (tituloPost),
+//     autor :(autorPost),
+//     conteudo : (conteudoPost)
+//     }
+    
+//     containerPost.innerHTML += `<p>${tituloPost.value}</p> <p>${autorPost.value}</p><p>${conteudoPost.value}</p>`
+    
+
+    
+ 
