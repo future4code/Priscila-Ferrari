21302b480f3700fcd09fd644e586f53a18cd3639
Priscila Carvalho <priferrari08@gmail.com>
2021-05-02 17:16:16 -0300

    Exercicio 5

--- semana4/lista-exercicios/exercicios.js
+++ semana4/lista-exercicios/exercicios.js
@@ -56,6 +56,9 @@ function retornaMaiorNumero(array) {
 
 function retornaQuantidadeElementos (array) {
    // implemente sua lógica aqui
+   let qtdNumeros = array.length
+     
+   return qtdNumeros
 }
 
 //Exercício 6
