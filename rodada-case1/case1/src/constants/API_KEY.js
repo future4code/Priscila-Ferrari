052a94487585f2ccbc1969dba96aa509bb5dbdfc
Priscila Carvalho <priferrari08@gmail.com>
2021-10-27 19:49:27 -0300

    feito lista com a api key

---
+++ rodada-case1/case1/src/constants/API_KEY.js
@@ -0,0 +1,3 @@
+export const API_KEY = "96189593dd8a6085d10a0a2190357e64"
+
+// clumsy-ground.surge.sh
--- rodada-case1/case1/src/pages/HomePage/HomePage.js
+++ rodada-case1/case1/src/pages/HomePage/HomePage.js
@@ -3,20 +3,28 @@ import Card from "../../components/Card/Card";
 import Header from "../../components/Header/Header";
 import useRequestData from "../../hooks/useRequestData";
 import { MoviesContainer } from "./styled";
+import { BASE_URL } from "../../constants/urls";
+import { API_KEY } from "../../constants/API_KEY";
 
 const HomePage = () => {
 
+    const movies = useRequestData([],`${BASE_URL}/popular?api_key=${API_KEY}`)
+    console.log(movies)
+
+   
     
+
     return(
         <div>
         <Header />
-        <MoviesContainer>
+        <MoviesContainer> 
            <Card 
            title={"Batata"}
            image={"https://picsum.photos/200"}
            onClick={() => null}
            />
-        </MoviesContainer>
+           
+         </MoviesContainer>
         
         </div>
     );
